6bca458fd0d89e92183b3d3fc72eb8084ec87159
royzheng0410 <royzheng0410@gmail.com>
2019-09-17 22:39:25 +0930

    episode 87 delete confirmation

--- wwwroot/js/CustomScript.js
+++ wwwroot/js/CustomScript.js
@@ -1,1 +1,11 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmSpan = 'confirmDeleteSpan_' + uniqueId;
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmSpan).hide();
+    }
+}
